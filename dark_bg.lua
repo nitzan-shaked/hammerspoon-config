e821bccd744a07bc3ee0d3ba78d591e8e25c38b4
Nitzan Shaked <nitzans@nextsilicon.com>
2023-05-16 15:23:03 +0300

    persist dark_bg across restarts

--- dark_bg.lua
+++ dark_bg.lua
@@ -1,61 +1,63 @@
 --[[ STATE ]]
 
-local cls = {
-	light_level=1,
-	canvas=nil,
-}
+local SETTINGS_KEY = "dark_bg"
+
+local light_level = hs.settings.get(SETTINGS_KEY .. ".light_level") or 1
+local canvas = nil
 
 --[[ LOGIC ]]
 
-cls.show = function ()
-	if cls.canvas then return end
+local function init_canvas ()
 	canvas = hs.canvas.new({})
-	canvas:appendElements({
-		type="rectangle",
-		frame={x=0, y=0, w="100%", h="100%"},
-		action="fill",
-		fillColor={red=0, green=0, blue=0, alpha=1 - cls.light_level},
-	})
 	canvas:level(hs.canvas.windowLevels.desktop)
 	canvas:_accessibilitySubrole("dark_bg")
-	cls.canvas = canvas
-	cls.refresh_screen_layout()
-	cls.canvas:show()
+	canvas:appendElements({
+		type="rectangle",
+		action="fill",
+		fillColor={red=0, green=0, blue=0},
+	})
 end
 
-cls.hide = function ()
-	if not cls.canvas then return end
-	cls.canvas:delete()
-	cls.canvas = nil
+local function refresh_canvas_layout ()
+	canvas:frame(hs.screen.mainScreen():frame())
 end
 
-cls.refresh_screen_layout = function ()
-	if not cls.canvas then return end
-	cls.canvas:frame(hs.screen.mainScreen():frame())
-end
+local function set_light_level (new_light_level)
+	light_level = (
+		new_light_level < 0 and 0
+		or new_light_level > 1 and 1
+		or new_light_level
+	)
 
-cls.set_light_level = function (light_level)
-	cls.light_level = light_level < 0 and 0 or light_level > 1 and 1 or light_level
-
-	if cls.light_level == 1 then
-		cls.hide()
-	elseif not cls.canvas then
-		cls.show()
+	canvas:alpha(1 - light_level)
+	if light_level == 1 then
+		canvas:hide()
 	else
-		cls.canvas[1].fillColor.alpha = 1 - cls.light_level
+		canvas:show()
 	end
+	hs.settings.set(SETTINGS_KEY .. ".light_level", light_level)
 end
 
-cls.darker = function ()
-	cls.set_light_level(cls.light_level - 0.1)
+local function darker ()
+	set_light_level(light_level - 0.1)
 end
 
-cls.lighter = function ()
-	cls.set_light_level(cls.light_level + 0.1)
+local function lighter ()
+	set_light_level(light_level + 0.1)
 end
 
-cls.watcher = hs.screen.watcher.new(cls.refresh_screen_layout)
+--[[ INIT ]]
+
+init_canvas()
+refresh_canvas_layout()
+set_light_level(light_level)
+
+local watcher = hs.screen.watcher.new(refresh_canvas_layout)
 
 --[[ MODULE ]]
 
-return cls
+return {
+	set_light_level=set_light_level,
+	darker=darker,
+	lighter=lighter,
+}
